75ec8832e1ac5bc1c7d884ee256b4c31f35df812
Sam Roy <samroy92@live.com>
2017-12-15 13:41:32 -0800

    Update whackamole.ino

--- whackamole.ino
+++ whackamole.ino
@@ -2,11 +2,14 @@ int moles = 5;
 
 int leds[5];
 int buttons[5];
-bool isHit[5];
-int hitCount = 0;
-unsigned long time;
+
+//starting delay
 float moleHang = 3000;
+
+int badHits = 0;
 int score = 0;
+
+int prevRandom = 0;
 
 bool buttonState = false;
 bool lastButtonState = false;
@@ -19,63 +22,71 @@ void setup()
   {
     pinMode(9+i, OUTPUT);
     pinMode(3+i, INPUT);
-    
-    isHit[i]=true;
   }
 }
 
 void loop()
 {
+  // way to time how long the led is on
   int count = 0;
-  time = millis();
-  int random = randLed();
+  //generate random number
+  int random = genRand();
+  
+  //set led(mole) to on based on random number, and corresponding button
   int curLed = random + 9;
   int curButton = random + 3;
   
+  //while the time of moleHang(in ms) is not past
   while(count < moleHang)
   {
+    //turn on the mole every iteration
 	digitalWrite(curLed, true);
     
+    //check if currect button is true
     if(digitalRead(curButton))
   	{
+      //increment score
       score++;
-      moleHang = moleHang * 0.90;
       
+      //speed up game after a successful mole hit
+      moleHang = moleHang * 0.95;
+      
+      //break out of while and start new loop()
       break;
-  	} 
-       
+  	}
+    
+    //time in ms per unit of moleHang
     delay(1);
     count++;
   }
   
-  
-  /*for(int i = 0; i < moles; i++)
-  {
-    if(digitalRead(3+i))
-    {
-      digitalWrite(9+i, true);
-      hitCount++;
-    }
-  }*/
-    
-  /*digitalWrite(13, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(13, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
-  */
-	reset();
+  //reset game clear board
+  reset();
 }
 
 void reset()
 {
+  //loop through all buttons and lights set all to false
   for(int i = 0; i < moles; i++)
   {
     digitalWrite(9+i, false);
-    //digitalWrite(3+i, false);
+    digitalWrite(3+i, false);
   }
 }
 
-int randLed()
+//generate random number from 0 to how many moles in play
+int genRand()
 {
   return random(0, moles);
+}
+
+//mock function for a gameover scenario
+int gameover()
+{
+  for(int i = 0; i < moles; i++)
+  {
+    digitalWrite(9+i, true);
+  }
+  
+  return 0;
 }
